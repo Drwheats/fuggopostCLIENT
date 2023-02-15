a95161dff38e9500ca1691b4924727b885ce0486
Drwheats <Dr.Wheats@gmail.com>
2023-02-15 00:02:49 -0500

    Fixed mobile, fixed formatting etc

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,9 @@ export default function Home() {
         <div>
 
             <div className="content">
-                <h1>Welcome to Fuggo's post world!</h1>
+                <h1>Welcome to Ben dot Place!</h1>
                 <h2>Select a board and post away!</h2>
-                <a style={{fontSize: 200}} href="/b">/b</a>
+                <a style={{fontSize: 200}} href="/b">/b/</a>
             </div>
         </div>
 
