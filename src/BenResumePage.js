dc89d810516e1508d697cfeca22500551ba63fcc
gondola <dr.wheats@gmail.com>
2023-11-28 19:52:23 -0500

    added portfolio page

--- src/BenResumePage.js
+++ src/BenResumePage.js
@@ -1,33 +1,62 @@
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from "react-pro-sidebar";import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import ReceiptRoundedIcon from "@mui/icons-material/ReceiptRounded";
+import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
+import TimelineRoundedIcon from "@mui/icons-material/TimelineRounded";
+import BubbleChartRoundedIcon from "@mui/icons-material/BubbleChartRounded";
+import WalletRoundedIcon from "@mui/icons-material/WalletRounded";
+import AccountBalanceRoundedIcon from "@mui/icons-material/AccountBalanceRounded";
+import SavingsRoundedIcon from "@mui/icons-material/SavingsRounded";
+import MonetizationOnRoundedIcon from "@mui/icons-material/MonetizationOnRounded";
+import SettingsApplicationsRoundedIcon from "@mui/icons-material/SettingsApplicationsRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import ShieldRoundedIcon from "@mui/icons-material/ShieldRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+
 export default function BenResumePage() {
-
-    function w3_open() {
-        document.getElementById("main").style.marginLeft = "25%";
-        document.getElementById("mySidebar").style.width = "25%";
-        document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("openNav").style.display = 'none';
-    }
-
-    function w3_close() {
-        document.getElementById("main").style.marginLeft = "0%";
-        document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("openNav").style.display = "inline-block";
-}
+    // const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } =
+    //     useProSidebar();
     return (
         <body className="homePageBody">
-        <div className="w3-sidebar w3-bar-block">
-            <a href="#" className="w3-bar-item w3-button">Link 1</a>
-            <a href="#" className="w3-bar-item w3-button">Link 2</a>
-            <a href="#" className="w3-bar-item w3-button">Link 3</a>
-        </div>
+        <div className="portfolioPage">
 
-        <div>
-            ... page content ...
+        <div className="sideBarStyles">
+
+            <Sidebar className="sideBar">
+                <Menu>
+                    <MenuItem className="menu1" icon={<MenuRoundedIcon />}>
+                        <h2> Ben G</h2>
+                    </MenuItem>
+                    <MenuItem icon={<GridViewRoundedIcon />}> Home </MenuItem>
+                    <MenuItem icon={<ReceiptRoundedIcon />}> About </MenuItem>
+                    <SubMenu label="Projects" icon={<BarChartRoundedIcon />}>
+                        <MenuItem icon={<TimelineRoundedIcon />}> Writing Project </MenuItem>
+                        <MenuItem icon={<BubbleChartRoundedIcon />}>iFixit Project</MenuItem>
+                    </SubMenu>
+                    <SubMenu label="Skills" icon={<WalletRoundedIcon />}>
+                        <MenuItem icon={<AccountBalanceRoundedIcon />}>
+                            More Skills
+                        </MenuItem>
+                        <MenuItem icon={<SavingsRoundedIcon />}>Skills</MenuItem>
+                    </SubMenu>
+                    <MenuItem icon={<MonetizationOnRoundedIcon />}>Bibliography</MenuItem>
+                    <SubMenu label="Sources" icon={<SettingsApplicationsRoundedIcon />}>
+                        <MenuItem icon={<AccountCircleRoundedIcon />}> Ben </MenuItem>
+                        <MenuItem icon={<ShieldRoundedIcon />}> Ben </MenuItem>
+                        <MenuItem icon={<NotificationsRoundedIcon />}>
+                            WRIT 2201
+                        </MenuItem>
+                    </SubMenu>
+                    <MenuItem icon={<LogoutRoundedIcon />}> Help </MenuItem>
+                </Menu>
+            </Sidebar>
         </div>
         <div>
 
             <div className="homePageHeader">
-                <h1 className="homePageName">Ben Gallagher's ePortfolio</h1>
 
+                <h1 className="homePageName">Ben Gallagher's ePortfolio</h1>
                 <br/>
                 <img className="portfolioPics1" src={"./APITEC1.jpeg"} alt={"AI generated images are already getting boring"} width="500" height="500"/>
 
@@ -49,7 +78,7 @@ export default function BenResumePage() {
                 <h1 className="homePageProjects">Projects</h1>
             </div>
             <div className="projectHolder">
-                <a href={"https://github.com/drwheats/"}>
+                <a href={"#"}>
                     <div className="project">
                         <img className="projectImg" src="/questionmark.png"></img>
                         <div className="projectContainer">
@@ -58,40 +87,42 @@ export default function BenResumePage() {
                         </div>
                     </div>
                 </a>
-                <a href={"https://www.ifixit.com/User/4387835/Ben+G"}>
+                <a href={""}>
                     <div className="project">
                         <img className="projectImg" src="/questionmark.png"></img>
                         <div className="projectContainer">
                             <h4><b>Two</b></h4>
-                            <p>This is project number two, along with the skills that I learned from <it className=""></it></p>
+                            <p>This is project number two, along with the skills that I learned from it </p>
                         </div>
                     </div>
                 </a>
 
             </div>
+            <div className="projectHolder">
+                <a href={""}>
+                    <div className="project">
+                        <img className="projectImg" src="/questionmark.png"></img>
+                        <div className="projectContainer">
+                            <h4><b>Three</b></h4>
+                            <p>This is project number 3 - and these are the writing skills that it exhibits.</p>
+                        </div>
+                    </div>
+                </a>
+                <a href={""}>
+                    <div className="project">
+                        <img className="projectImg" src="/questionmark.png"></img>
+                        <div className="projectContainer">
+                            <h4><b>Four</b></h4>
+                            <p>If I learned 4 things, the 4th thing will go here. </p>
+                        </div>
+                    </div>
+                </a>
+
             </div>
+
+        </div>
         <div>
         </div>
-        <h1 className="homePageProjects">Links</h1>
-        <div className="projectHolder">
-            <a href={"https://github.com/drwheats/"}>
-                <div className="project">
-                    <img className="projectImg" src="/gitcat.png"></img>
-                    <div className="projectContainer">
-                        <h4><b>GitHub</b></h4>
-                        <p>This is my GitHub account. I mostly program in Javascript and in Python. Graphic design is my passion.</p>
-                    </div>
-                </div>
-            </a>
-            <a href={"https://www.ifixit.com/User/4387835/Ben+G"}>
-                <div className="project">
-                    <img className="projectImg" src="/ifixit-logo-png-transparent.png"></img>
-                    <div className="projectContainer">
-                        <h4><b>iFixit</b></h4>
-                        <p>This is my iFixit account. I specialize in repair guides for old electronics and micromobility.</p>
-                    </div>
-                </div>
-            </a>
 
         </div>
 
